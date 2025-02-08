a15de83e24e75c3bc41dbc8ad73542c4df608117
JackIzumihara <1551500700@k12.hi.us>
2025-02-08 22:06:00 +0000

    did everything in readme except for rangechange

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var resultParagraph = document.getElementById("result");
 var random;
 var range = 100;
 var nextRandom = Math.floor(Math.random() * range + 1);
+var score = 0;
 
 
 
@@ -16,12 +17,40 @@ function generateRandom() {
 
 // Function for the higher button
 function higher() {
-   
+    if (random > nextRandom) {
+        window.alert("Wrong, It isn't bigger!");
+        window.alert(nextRandom);
+      }
+      else if (random < nextRandom) {
+        window.alert("Correct, It is bigger!");
+        window.alert(nextRandom);
+        score = score + 1;
+        window.alert("score is now:");
+        window.alert(score);
+      }
+      else {
+        window.alert("They are the same!");
+      }
+      generateRandom()
 }
 
 // Function for the lower button
 function lower() {
-   
+    if (random < nextRandom) {
+        window.alert("Wrong, It isn't smaller!");
+        window.alert(nextRandom);
+      }
+      else if (random > nextRandom) {
+        window.alert("Correct, It is smaller!");
+        window.alert(nextRandom);
+        score = score + 1;
+        window.alert("score is now:");
+        window.alert(score);
+      }
+      else {
+        window.alert("They are the same!");
+      }
+      generateRandom()
 }
 
 //Generate a number to start the game!
